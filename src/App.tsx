b0025877252e94101938b8bde7715fbf16d4de90
ragava 0125 <ragava0125@gmail.com>
2024-07-26 06:56:45 +0000

    change ss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Student App</p>
+        <p>Student ss</p>
       </header>
     </div>
   );
