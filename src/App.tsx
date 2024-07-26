8c1c8ea83da2880d66f61c806c1f4db64a9c3405
ragava 0125 <ragava0125@gmail.com>
2024-07-26 16:25:10 +0000

    front-page

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,28 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Student ss</p>
+    <div>
+      <header className="App">
+        <h1>Student</h1>
       </header>
+      <body>
+        <div className="in">
+          <label>Student name:</label>
+          <input type="text" required />
+          <label>STD:</label>
+          <input type="number" required />
+          <button className="btn">Submit</button>
+        </div>
+      </body>
+      <div>
+        <nav className="vertical-navbar">
+          <ul>
+            <ol>Student</ol>
+            <ol>Mark list</ol>
+            <ol>Rank</ol>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
